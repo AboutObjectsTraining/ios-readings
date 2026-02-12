8e32db9df9166ed82d38374e4ac7bb281e568539
Jonathan Lehr <jonathan@aboutobjects.com>
2026-02-12 11:18:22 -0500

    Initial Commit

---
+++ Readings/ReadingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingsApp.swift
+//  Readings
+//
+//  Created by Jonathan Lehr on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
